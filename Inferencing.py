093bf693752334b15a81b6001560118ba51231a5
Dn-iel <danielsnow229@gmail.com>
2025-06-06 16:27:45 +0700

    Update Inferencing.py

--- Inferencing.py
+++ Inferencing.py
@@ -22,17 +22,24 @@ indices = model_data["indices"]
 netflix_df = model_data["netflix_title"]
 content_recommender = model_data["content_recommender"]
 
+# Kolom yang akan ditampilkan
+columns_to_show = [
+    'type', 'title', 'director', 'cast', 'country', 'date_added',
+    'release_year', 'rating', 'listed_in', 'description',
+    'duration_minutes', 'duration_seasons'
+]
+
 # UI
 st.title("🎬 Netflix Movie Recommender")
 st.markdown("Enter a Netflix movie title below to get detailed information and similar movie recommendations.")
 
 title = st.text_input("Enter a movie title:")
-search_clicked = st.button("Get Recommended Movies")  # Tombol SELALU muncul
+search_clicked = st.button("Get Recommended Movies")
 
 if search_clicked and title:
     if title in indices:
         index = indices[title]
-        movie_details_df = netflix_df.loc[[index]]  # Ini hasilnya pasti DataFrame
+        movie_details_df = netflix_df.loc[[index], columns_to_show]
         st.subheader("🎥 Selected Movie Details")
         st.table(movie_details_df)
 
@@ -43,7 +50,7 @@ if search_clicked and title:
             with st.expander(f"{i}. {rec_title}"):
                 if rec_title in indices:
                     rec_index = indices[rec_title]
-                    rec_details_df = netflix_df.loc[[rec_index]]
+                    rec_details_df = netflix_df.loc[[rec_index], columns_to_show]
                     st.table(rec_details_df)
                 else:
                     st.warning(f"Details for '{rec_title}' not found.")
